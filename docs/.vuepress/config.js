8ea71e07549162a956e3e362bba54386959c532e
DaniilAlyeksyeyenko <da295615@gmail.com>
2023-01-07 20:23:49 +0200

    deleted morochenec

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -76,7 +76,7 @@ module.exports = {
 
     lastUpdated: 'Останнє оновлення', // string | boolean
       // Assumes GitHub. Can also be a full GitLab url.
-    repo: 'https://github.com/LLumisS/media-content-analyze-system',
+    repo: 'https://github.com/Tutinichic/media-content-analyze-system',
     // Customising the header label
     // Defaults to "GitHub"/"GitLab"/"Bitbucket" depending on `themeConfig.repo`
     repoLabel: 'Github',
